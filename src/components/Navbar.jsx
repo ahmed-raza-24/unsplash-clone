9bd0545b97fe0736d37c6dd1e9c82af043183487
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-02-27 20:11:43 +0530

    Navbar updated features

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,21 @@ const Navbar = () => {
   return (
     <div>
         <div className='Navbar'>
-            <input type='search' placeholder='Search photo and illustrate' />
-            <NavLink to="/getUnsplash" >GetUnsplash</NavLink>
+            <input type='search' placeholder='Search photos and illustrate' />
+            <NavLink to="/getUnsplash" >GetUnsplash+</NavLink>
             <NavLink to="/login" >Login</NavLink>
             <button className='Submit'>Submit an image</button>
         </div>
+        <div className='section'>
+              <NavLink to="/Featured">Featured</NavLink>
+              <NavLink to="/Wallpapers">Wallpapers</NavLink>
+              <NavLink to="/3DRenders">3D Renders</NavLink>
+              <NavLink to="/Nature">Nature</NavLink>
+              <NavLink to="/Texture">Texture</NavLink>
+              <NavLink to="/Film">Film</NavLink>
+              <NavLink to="/Architecture">Architecture</NavLink>
+              <NavLink to="/StreetPhotography">StreePhotography</NavLink>
+          </div>
     </div>
   )
 }
