467027a24c78a275ded94be939308990afcb276e
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-03-01 21:02:19 +0530

    fixed page layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,24 @@ import Featured from './pages/Featured'
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Featured />
+    <div className="layout">
+
+      {/* Sidebar */}
+      <div className="sidebar">
+        <Navbar />
+      </div>
+
+      {/* Main Content */}
+      <div className="main">
+        <div className="topbar">
+          <Navbar />
+        </div>
+
+        <div className="pageContent">
+          <Featured />
+        </div>
+      </div>
+
     </div>
   )
 }
